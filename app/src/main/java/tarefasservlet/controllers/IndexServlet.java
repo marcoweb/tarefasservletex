20965d640278310bd25cf002bcd5c77536c0c2aa
marcoweb <marcocarvalho.web@gmail.com>
2023-04-24 16:35:41 +0000

    Servlet

---
+++ app/src/main/java/tarefasservlet/controllers/IndexServlet.java
@@ -0,0 +1,39 @@
+package tarefasservlet.controllers;
+
+import java.util.Hashtable;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import tarefasservlet.models.Tarefa;
+
+@WebServlet(name = "IndexServlet", urlPatterns= "/")
+public class IndexServlet extends HttpServlet {
+    private Hashtable<Integer, Tarefa> tarefas = new Hashtable<Integer, Tarefa>();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+            Tarefa tarefa = new Tarefa();
+            tarefa.setId(1);
+            tarefa.setDescricao("Fazer a Tarefa");
+            tarefas.put(tarefa.getId(), tarefa);
+            request.setAttribute("tarefas", tarefas);
+            request.setAttribute("mensagem", "Olá Servlet!!!");
+            request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+            Tarefa tarefa = new Tarefa();
+            tarefa.setId(tarefas.size() + 1);
+            tarefa.setDescricao(request.getParameter("descricao"));
+            tarefas.put(tarefa.getId(), tarefa);
+            //request.getRequestDispatcher("/").forward(request, response);
+            response.sendRedirect("/");
+    }
+}
---
+++ app/src/main/java/tarefasservlet/models/Tarefa.java
@@ -0,0 +1,20 @@
+package tarefasservlet.models;
+
+public class Tarefa {
+    private int id;
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
